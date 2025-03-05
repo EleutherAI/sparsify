Correct KL divergence values

sparsify/sparse_coder.py
@@ -56,10 +56,7 @@ class SparseCoder(nn.Module):
         self.d_in = d_in
         self.num_latents = cfg.num_latents or d_in * cfg.expansion_factor
 
-        if self.cfg.optimized_encoder_config is None:
-            self.encoder = nn.Linear(d_in, self.num_latents, device=device, dtype=dtype)
-            self.encoder.bias.data.zero_()
-        else:
+        if self.cfg.optimized_encoder_config.is_not_none:
             self.encoder = self.cfg.optimized_encoder_config.build_encoder(
                 d_in,
                 self.num_latents,
@@ -68,6 +65,9 @@ class SparseCoder(nn.Module):
                 pkm_config=self.cfg.pkm_config,
                 kron_config=self.cfg.kronecker_config,
             )
+        else:
+            self.encoder = nn.Linear(d_in, self.num_latents, device=device, dtype=dtype)
+            self.encoder.bias.data.zero_()
 
         if decoder:
             # Transcoder initialization: use zeros
@@ -228,7 +228,7 @@ class SparseCoder(nn.Module):
 
     def encode(self, x: Tensor) -> EncoderOutput:
         """Encode the input and select the top-k latents."""
-        if self.cfg.optimized_encoder_config is not None:
+        if self.cfg.optimized_encoder_config.is_not_none:
             sae_in = self.input_preprocess(x)
             return self.encoder.topk(sae_in, self.cfg.k)
         return self.select_topk(self.pre_acts(x))

sparsify/trainer.py
@@ -415,6 +415,14 @@ class Trainer:
         for name, sae in self.saes.items():
             sae.cfg.k = k
 
+        @torch.compile(mode="reduce-overhead", fullgraph=True)
+        def kl_loss(clean_logits, dirty_lps):
+            return torch.sum(
+                torch.softmax(clean_logits, dim=-1)
+                * (torch.log_softmax(clean_logits, dim=-1) - dirty_lps),
+                dim=-1,
+            ).mean()
+
         for batch in dl:
             torch.compiler.cudagraph_mark_step_begin()
 
@@ -438,11 +446,10 @@ class Trainer:
             num_tokens_in_step += N
 
             # Compute clean logits if using KL loss
-            clean_probs = (
-                self.model(x).logits.softmax(dim=-1)
-                if self.cfg.loss_fn == "kl"
-                else None
-            )
+            with torch.inference_mode():
+                clean_logits = (
+                    self.model(x).logits if self.cfg.loss_fn == "kl" else None
+                )
 
             # Forward pass on the model to get the next batch of activations
             handles = [
@@ -456,9 +463,11 @@ class Trainer:
 
                         avg_ce += float(self.maybe_all_reduce(ce.detach()) / denom)
                     case "kl":
+                        assert clean_logits is not None
                         dirty_lps = self.model(x).logits.log_softmax(dim=-1)
-                        kl = -torch.sum(clean_probs * dirty_lps, dim=-1).mean()
+                        kl = kl_loss(clean_logits.clone().detach(), dirty_lps)
                         kl.div(acc_steps).backward()
+                        del clean_logits
 
                         # Also compute the cross entropy loss for logging purposes
                         with torch.inference_mode():