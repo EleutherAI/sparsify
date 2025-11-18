e9b2a6655013fd528554838229ca5ef38f96d57f
Lucia Quirke <luciarosequirke@gmail.com>
2025-11-18 04:33:09 +0000

    Update num proc handling to prevent OOM

--- sparsify/data.py
+++ sparsify/data.py
@@ -18,7 +18,7 @@ def chunk_and_tokenize(
     tokenizer: PreTrainedTokenizerBase,
     *,
     format: str = "torch",
-    num_proc: int = min(48, cpu_count() // 2),
+    num_proc: int = min(64, cpu_count() // 2),
     text_key: str = "text",
     max_seq_len: int = 2048,
     return_final_batch: bool = False,
